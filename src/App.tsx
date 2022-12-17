697d56ad3546e4c4ecf75fe9a7a27e704250ec2d
Mary Skywalker <snowdennaruto90@gmail.com>
2022-12-16 20:39:48 -0400

    added className container__app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from './components/Header';
+import { Search } from './components/Search';
 import './styles/global.scss';
 
 export function App() {
   return (
     <>
       <Header/>
+      <div className="container__app">
+        <Search />
+      </div>
     </>
   )
 }
