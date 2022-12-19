18f079e44a81c309bd1c2fc243abcb184fcc4eb3
Mary Skywalker <snowdennaruto90@gmail.com>
2022-12-18 23:54:40 -0400

    added component card

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,25 @@
+import { PersonType } from '../../@types/Person';
+import styles from './styles.module.scss';
+
+export function Card({ characters }: PersonType){
+    return(
+        <div>
+            <div>
+                <img src={characters.results.image} alt={characters.results.name} />
+            </div>
+            <div>
+                <h1>{characters.results.name}</h1>
+                <div>
+                    <span>{characters.results.status}</span>
+                    <p>{characters.results.species}</p>
+                </div>
+
+                <h2>Origirem:</h2>
+                <p>{characters.results.origin.name}</p>
+                
+                <h2>Localização</h2>
+                <p>{characters.results.location.name}</p>
+            </div>
+        </div>
+    );
+}
