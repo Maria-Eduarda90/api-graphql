d51b9896a253b7f20db338d222f172817dce17d0
Mary Skywalker <snowdennaruto90@gmail.com>
2022-12-19 00:00:55 -0400

    mapping character information

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import { INFO_PERSON } from './graphql';
+import { useQuery } from "@apollo/client";
+
 import { Header } from './components/Header';
 import { Search } from './components/Search';
 import './styles/global.scss';
+import { PersonType } from './@types/Person';
+import { Card } from './components/Card';
 
 export function App() {
+  const { loading, error, data } = useQuery<{ characters: PersonType[] }>(INFO_PERSON);
+  console.log(data)
+  
+  if(loading) {
+    return <p>Loading...</p>
+  }
+  
   return (
     <>
       <Header/>
       <div className="container__app">
         <Search />
       </div>
+      {data?.characters.map((person, index) => <Card key={index} characters={person.characters}/>
+      )}
     </>
   )
 }
